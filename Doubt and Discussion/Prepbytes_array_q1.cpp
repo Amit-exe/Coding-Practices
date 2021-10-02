3d1b3d2eda9da7fd7cd3308f205323c2ce0daad8
AMIT KUSHWAHA <amit.kushwaha@somaiya.edu>
2021-10-02 23:44:38 +0530

    Create Prepbytes_array_q1.cpp
    Guys try this question also review my code and search logical error since it's clearing sample test cases but failing during submission.

---
+++ Doubt and Discussion/Prepbytes_array_q1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//https://mycode.prepbytes.com/problems/arrays/MINMAX1 (Question link)
+
+int main()
+{
+  //write your code here
+  int t;
+  cin>>t;
+  while(t--){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+      cin>>a[i];
+    }
+    int mi=10000000,ma=0;
+    for(int i=0;i<n;i++){
+      if(a[i]<mi) mi = a[i];
+      if(a[i]>ma) ma = a[i];
+      
+    }
+    cout<<mi<<" "<<ma<<"\n";
+  }
+  return 0;
+}
